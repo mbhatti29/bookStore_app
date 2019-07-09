b6d01197ed392e3d79b45caf96c7f857e1f074ff
Mudassar Bhatti <mbhatti000@citymail.cuny.edu>
2019-07-08 20:58:02 -0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
-import png from './trans-book.png'
+// import logo from './logo.svg';
+// import png from './trans-book.png'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={png} className="App-logo" alt="logo" />
-        <p>
-          Book Store
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      search: '',
+      books: [],
+      library: [],
+    }
+  }
+  
+
+  componentDidMount() {
+  
+  }
+
+  searchValue = (event) => {
+    this.setState({
+      search : event.target.value
+    })
+  }
+
+  searchBookInput = (event) => {
+    event.preventDefault()
+
+    this.state.search && 
+    fetch("https://www.googleapis.com/books/v1/volumes?q=" + this.state.search)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          books: res.items
+        })
+      })
+
+  }
+
+  render() {
+
+    const booksMapped = this.state.books.map((book, i) => (
+    <div key={i} onClick={this.addToLibrary}>
+      <p>{book.volumeInfo.title}</p>
+      <img src={book.volumeInfo.imageLinks.thumbnail} alt='thumbnail'/>
+    </div>))
+
+    return (
+      <div>
+        <form onSubmit={this.searchBookInput}>
+          <input onChange={this.searchValue}></input>
+          <button>Submit</button>
+        </form>
+        <div className='bookDiv'>
+          {booksMapped}
+        </div>
+      </div>
+    )
+  }
 }
+
 
 export default App;
